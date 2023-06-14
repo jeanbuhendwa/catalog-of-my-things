dba35e8c7dd5238eeb8a711fb461b79972569254
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-06-14 10:12:07 +0500

    Add Book & Label Classes

---
+++ label.rb
@@ -0,0 +1,15 @@
+class Label
+  attr_accessor :id, :title, :color, :items
+
+  def initialize(title, color)
+    @id = rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def item_add(item)
+    @items << item
+    item.label = self
+  end
+end
