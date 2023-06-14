7790ee3f903cb0fa6f75de1b7e0c51782f354b9f
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-06-14 17:47:44 +0500

    Add Unit Testing For book.rb & label.rb

---
+++ spec/label_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require_relative '../item'
+require_relative '../label'
+
+RSpec.describe Label do
+  describe '#item_add' do
+    it 'adds the item to the label' do
+      label = Label.new('My Label', 'red')
+      item = double('Item')
+      expect(item).to receive(:label=).with(label)
+      label.item_add(item)
+      expect(label.items).to include(item)
+    end
+  end
+end
