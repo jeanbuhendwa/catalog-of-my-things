b14618961233909b3fa0c270a49a210ea755a949
Syed Atril <atril3123@gmail.com>
2023-06-16 21:16:58 +0500

    Update label_spec.rb

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -3,6 +3,34 @@ require_relative '../item'
 require_relative '../Book/label'
 
 RSpec.describe Label do
+  let(:title) { 'Sample Title' }
+  let(:color) { 'red' }
+
+  describe '#initialize' do
+    it 'assigns a random ID' do
+      label = Label.new(title, color)
+      expect(label.id).to be_an(Integer)
+      expect(label.id).to be >= 1
+      expect(label.id).to be <= 1000
+    end
+
+    it 'assigns the provided title' do
+      label = Label.new(title, color)
+      expect(label.title).to eq(title)
+    end
+
+    it 'assigns the provided color' do
+      label = Label.new(title, color)
+      expect(label.color).to eq(color)
+    end
+
+    it 'initializes an empty array for items' do
+      label = Label.new(title, color)
+      expect(label.items).to be_an(Array)
+      expect(label.items).to be_empty
+    end
+  end
+
   describe '#item_add' do
     it 'adds the item to the label' do
       label = Label.new('My Label', 'red')
